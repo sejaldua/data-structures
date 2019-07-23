75f069f16b57931583003cbc51d159e459c16e7f
Sejal <sejal.dua@tufts.edu>
2019-07-23 09:27:10 -0400

    uploading tests for COMP 15 hw1

---
+++ checkhw1/Pirate.cpp
@@ -0,0 +1,64 @@
+/*
+ * Pirate.cpp
+ * COMP15
+ * Fall 2018
+ *
+ * Implarrrrrrmentation of a Pirate
+ */
+
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include "Pirate.h"
+using namespace std;
+
+int Pirate::pirate_count = 0;
+
+Pirate::Pirate()
+{
+    pirate_id = -1;
+    name = "Cap'n Hook";
+}
+
+Pirate::Pirate(string n)
+{
+    assign_pirate_id();
+    name = n;
+}
+
+void Pirate::print() const
+{
+    cout << pirate_id << " "
+         << name;
+}
+
+void Pirate::generate_pirate_name(ifstream &infile, int range)
+{
+    string pname;
+    int rando = rand() % range;
+    int count = 0;
+
+    infile.clear();
+    infile.seekg(0, ios::beg);
+    while (getline(infile, pname))
+    {
+        if (count == rando)
+        {
+            name = pname;
+            break;
+        }
+        count++;
+    }
+}
+
+void Pirate::assign_pirate_id()
+{
+    pirate_id = pirate_count++;
+}
+
+bool operator == (const Pirate &p1, const Pirate &p2)
+{
+    return (p1.pirate_id == p2.pirate_id);
+}
+
